914198adfd4eedaa494339cc2c6bf4c844620b2a
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-12-02 10:51:00 -0600

    Added header to Header index.js

--- react-portfolio/src/components/Header/index.js
+++ react-portfolio/src/components/Header/index.js
@@ -1,3 +1,11 @@
-// <!-- <header className="App-header">
-    
-//       </header> -->
+import React from "react";
+
+function Header() {
+  return (
+    <div className="container">
+      <header>Lindsey</header>
+    </div>
+  );
+}
+
+export default Header;
