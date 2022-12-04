9898df1294262235b262e82d49ec8e9ff7d76568
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-12-04 09:25:16 -0600

    Added projects and portfolio info.

--- react-portfolio/src/components/Portfolio/index.jsx
+++ react-portfolio/src/components/Portfolio/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Projects from "../Projects/index.jsx";
+import projects from "../../projects.json";
 
 function Portfolio() {
-  return <div className="container"></div>;
+  return (<div className="container">
+    <div className="row">
+      {projects.map(item => {
+        return (
+          <Projects info={item} />
+        )
+      })}
+    </div>
+  </div>
+  )
 }
 
 export default Portfolio;
--- react-portfolio/src/components/Projects/index.jsx
+++ react-portfolio/src/components/Projects/index.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-function Projects() {
-  return <div className="container"></div>;
+function Projects(props) {
+  return (
+    <div className="container col-xl-4 col-lg-6 col-md-12">
+      <div className="card">
+        <img
+          src={props.info.image}
+          className="card-img-top"
+          alt={props.info.name}
+        />
+        <div className="card-body">
+          <h4 className="card-title">{props.info.name}</h4>
+          <p className="card-text">{props.info.description}</p>
+          <p className="card-text">{props.info.technologies}</p>
+          <a href={props.info.github} target="_blank" rel="noreferrer">
+            GitHub
+          </a>{" "}
+          |{" "}
+          <a href={props.info.deployed} target="_blank" rel="noreferrer">
+            Deployed
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
