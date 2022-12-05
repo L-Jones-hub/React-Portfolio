ec38b4752fd7adcbc9dc659a291c5bbbec40cc1d
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-12-05 09:49:03 -0600

    Styled nav

--- react-portfolio/src/components/Nav/index.jsx
+++ react-portfolio/src/components/Nav/index.jsx
@@ -1,25 +1,30 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
 function Nav(props) {
   const { pages, setCurrentPage, currentPage } = props;
-console.log(props)
+  console.log(props);
 
   useEffect(() => {
     document.title = currentPage?.name;
   }, [currentPage]);
 
   return (
-    <nav>
-      <ul className="flex-row">
-        {pages?.map((page) => (
-          <li
-            className={`mx-5 ${currentPage.name === page.name && "navActive"}`}
-            key={page.name}
-          >
-            <span onClick={() => setCurrentPage(page)}>{page.name}</span>
-          </li>
-        ))}
-      </ul>
+    <nav className="navbar flex-row bg-dark text-white">
+      <div className="container-fluid">
+        <h3>Lindsey Jones</h3>
+        <ul className="navbar-nav list-group-horizontal">
+          {pages?.map((page) => (
+            <li
+              className={`mx-5 nav-item ${
+                currentPage.name === page.name && "navActive"
+              }`}
+              key={page.name}
+            >
+              <span onClick={() => setCurrentPage(page)}>{page.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 }
