a12d6307be21b3b22878b863e8f2dccefd525a16
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-12-05 09:15:56 -0600

    Fixed resume page

--- react-portfolio/src/components/Resume/index.jsx
+++ react-portfolio/src/components/Resume/index.jsx
@@ -3,14 +3,22 @@ import React from "react";
 function Resume() {
   return (
     <div className="container">
-      <div className="row">
+      <div className="flex-row">
         <div className="col-md-12">
-          <div className="embed-responsive embed-responsive-1by1">
-            <iframe
-              title="resume"
-              className="embed-responsive-item"
-              src="https://drive.google.com/file/d/1E-SYeRLpFTlJ4BREp0mr7TCJIKShVDOt/view?usp=sharing"
-            ></iframe>
+          <div>
+            <p className="justify-content-center">Click on resume to see pdf in Google Drive.</p>
+            <a
+              href="https://drive.google.com/file/d/1E-SYeRLpFTlJ4BREp0mr7TCJIKShVDOt/view?usp=sharing"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                title="Click to see pdf in Google Drive."
+                className="resume col-md-10"
+                src="../../images/Resume.png"
+                alt="Resume"
+              ></img>
+            </a>
           </div>
         </div>
       </div>
